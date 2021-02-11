7bb47811e356f03ee89496daad89774f11ca4495
Nathan Hietala <Nrhietala@gmail.com>
2021-02-11 12:43:29 -0600

    Deploy

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT || 9000;
+
+app.use("/api/*", (_, res) => {
+  res.json({ data: "👺 The API is ALIVE 👺" });
+});
 
 app.listen(port, () => {
   console.log(`Server live at ${port}`);
